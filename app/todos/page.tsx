f0022cb8548dfb6d3a3de07b9640136a581b13c9
ratwatte96 <rahulratwatte96@gmail.com>
2024-08-02 18:18:19 +0100

    Added Persmission table and Update todos page to display todos

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -5,6 +5,28 @@ const AddTodoPage = () => {
   const [description, setDescription] = useState("");
   const [done, setDone] = useState(false);
   const [message, setMessage] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const getToDos = async () => {
+    try {
+      const response = await fetch("/api/todos", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const todos = await response.json();
+      setTodos(todos);
+      setDone(false);
+    } catch (error: any) {
+      setMessage(`Error: ${error.message}`);
+    }
+  };
 
   const addTodo = async () => {
     try {
@@ -36,6 +58,17 @@ const AddTodoPage = () => {
 
   return (
     <div>
+      <div>
+        <h1>TODOS</h1>
+        <button onClick={getToDos}>Get todos</button>
+        <ul>
+          {todos.map((todo: any) => (
+            <>
+              <li>{todo.description}</li>
+            </>
+          ))}
+        </ul>
+      </div>
       <h1>Add Todo</h1>
       <form onSubmit={handleSubmit}>
         <div>
---
+++ prisma/migrations/20240802164920_add/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Permission" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Permission_pkey" PRIMARY KEY ("id")
+);
