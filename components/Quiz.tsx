994e089ec1c7b7b7e1fc4a1ede4caf506785d516
ratwatte96 <rahulratwatte96@gmail.com>
2024-08-04 10:44:03 +0100

    Added progress bar

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -34,8 +34,16 @@ const Quiz: React.FC<QuizProps> = ({ steps }) => {
     );
   }
 
+  const progress = ((currentStep + 1) / steps.length) * 100;
+
   return (
     <div>
+      <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4">
+        <div
+          className="bg-skin-accent h-2.5 rounded-full"
+          style={{ width: `${progress}%` }}
+        ></div>
+      </div>
       <QuizStep
         {...steps[currentStep]}
         nextStep={nextStep}
