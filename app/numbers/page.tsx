c28956775cc866e780c97a201820ded70cca4222
ratwatte96 <rahulratwatte96@gmail.com>
2024-08-05 22:34:55 +0100

    Add missing type

--- app/numbers/page.tsx
+++ app/numbers/page.tsx
@@ -1,8 +1,9 @@
 import NewQuiz from "@/components/NewQuiz";
+import { NewQuizData } from "@/components/NewQuizStep";
 import { numbers } from "@/lib/quizItem";
 
 export default function Numbers() {
-  const numbersSteps: QuizData[] = numbers.sort(() => Math.random() - 0.5);
+  const numbersSteps: NewQuizData[] = numbers.sort(() => Math.random() - 0.5);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-skin-base text-skin-base">
