8d52af35538669612d66b6651cd3ba8984334da8
ratwatte96 <rahulratwatte96@gmail.com>
2024-08-14 06:33:32 +0100

    Updated consanants to new format

--- app/consonants/page.tsx
+++ app/consonants/page.tsx
@@ -1,13 +1,25 @@
-import Quiz from "@/components/Quiz";
-import { QuizData } from "@/components/QuizStep";
+import NewQuiz from "@/components/NewQuiz";
+import { NewQuizData } from "@/components/NewQuizStep";
 import { consonants } from "@/lib/quizItem";
 
 export default function Consonants() {
-  const consonantSteps: QuizData[] = consonants.sort(() => Math.random() - 0.5);
+  const consonantSteps: NewQuizData[] = consonants.sort(
+    () => Math.random() - 0.5
+  );
+  let lives = 100;
+  try {
+    fetch(`/api/lives`)
+      .then((res) => res.json())
+      .then((livesData) => {
+        lives = livesData.total_lives;
+      });
+  } catch (error: any) {
+    console.log(error);
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-skin-base">
-      <Quiz steps={consonantSteps} />
+      <NewQuiz steps={consonantSteps} startingLives={lives} />
     </main>
   );
 }
