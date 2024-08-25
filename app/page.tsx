4b43f50eeb528bc7b5e26363deb3c9881aff5426
ratwatte96 <rahulratwatte96@gmail.com>
2024-08-25 10:10:25 +0100

    Change home page

--- app/page.tsx
+++ app/page.tsx
@@ -21,100 +21,25 @@ export default function Home() {
       </div>
       <div className="flex flex-row flex-wrap w-80 sm:w-1/2 items-center justify-center">
         <button
-          onClick={() => handleRouting("vowels")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Vowels
-        </button>
-        <button
           onClick={() => handleRouting("consonants")}
           className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
         >
-          Consonants
+          Speak
         </button>
         <button
-          onClick={() => handleRouting("numbers/0")}
+          disabled
+          onClick={() => handleRouting("consonants")}
           className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
         >
-          Numbers (0-10)
+          Read
         </button>
+
         <button
-          onClick={() => handleRouting("numbers/1")}
+          disabled
+          onClick={() => handleRouting("vowels")}
           className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
         >
-          Numbers (10-20)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/2")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (20-30)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/3")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (30-40)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/4")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (40-50)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/5")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (50-60)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/6")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (60-70)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/7")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (70-80)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/8")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (80-90)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/9")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (90-100)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/10")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (100-1000)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/11")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (1000-10000)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/12")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (10000-20000)
-        </button>
-        <button
-          onClick={() => handleRouting("numbers/13")}
-          className="w-24 rounded-lg border border-skin-base m-4 px-3 py-1 text-xs text-skin-muted hover:text-skin-accent focus:outline-none sm:ml-2 sm:w-40 sm:text-base"
-        >
-          Numbers (30000-1000000)
+          Read & Speak
         </button>
       </div>
     </main>
