babc13d8cfb5013d37a1203a5973f0fb78b52050
ratwatte96 <rahulratwatte96@gmail.com>
2024-12-23 20:08:29 +0530

    Added basic custom quiz feature

--- components/CustomQuiz.tsx
+++ components/CustomQuiz.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import MultiSelectDropdown from "@/components/MultiSelectDropdown";
+import Quiz from "./Quiz";
 // import ChevronDownIcon from "../../public/chevron-down.svg";
 // import FilterIcon from "../../public/filter.svg";
 
@@ -10,43 +11,57 @@ interface CustomQuizProps {
 
 export const CustomQuiz: React.FC<CustomQuizProps> = ({ dropDownLetters }) => {
   const [selectedItems, setSelectedItems] = useState([]);
+  const [quizData, setQuizData] = useState([]);
 
-  const addTodo = async () => {
+  const getQuizData = async () => {
     try {
-      const response = await fetch("/api/todos", {
+      const response = await fetch("/api/quiz-data", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        // body: JSON.stringify({ description, done }),
+        body: JSON.stringify({ selectedItems }),
       });
 
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
 
-      const newTodo = await response.json();
+      const quizData = await response.json();
+      setQuizData(quizData);
     } catch (error: any) {}
   };
 
   return (
     <div className="flex flex-col">
-      <div>{...selectedItems}</div>
-      <MultiSelectDropdown
-        id="filters"
-        buttonLabel="Choose Letters"
-        items={dropDownLetters}
-        selectedItems={selectedItems}
-        setSelectedItems={setSelectedItems}
-        // iconStart={<FilterIcon />}
-        // iconEnd={<ChevronDownIcon />}
-      />
-      <button
-        className="flex items-center justify-center rounded-lg border border-skin-base px-2 py-1 text-xs font-medium text-skin-base focus:z-10 focus:outline-none focus:ring-4 sm:px-4 sm:py-2 sm:text-sm"
-        onClick={addTodo}
-      >
-        Start Quiz
-      </button>
+      {quizData.length === 0 ? (
+        <>
+          <div>{...selectedItems}</div>
+          <MultiSelectDropdown
+            id="filters"
+            buttonLabel="Choose Letters"
+            items={dropDownLetters}
+            selectedItems={selectedItems}
+            setSelectedItems={setSelectedItems}
+            // iconStart={<FilterIcon />}
+            // iconEnd={<ChevronDownIcon />}
+          />
+          <button
+            className="flex items-center justify-center rounded-lg border border-skin-base px-2 py-1 text-xs font-medium text-skin-base focus:z-10 focus:outline-none focus:ring-4 sm:px-4 sm:py-2 sm:text-sm"
+            onClick={getQuizData}
+          >
+            Start Quiz
+          </button>
+        </>
+      ) : (
+        <>
+          <Quiz
+            steps={quizData}
+            startingLives={100}
+            quiz_title={"Custom Quiz"}
+          />
+        </>
+      )}
     </div>
   );
 };
