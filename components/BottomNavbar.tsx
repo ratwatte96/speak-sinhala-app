dbf81f9c4060d4e9b584ca81f80b6bff7b2fa761
ratwatte96 <rahulratwatte96@gmail.com>
2025-02-19 19:00:48 +0530

    Improved bottom nav

--- components/BottomNavbar.tsx
+++ components/BottomNavbar.tsx
@@ -14,11 +14,11 @@ const BottomNavbar = () => {
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-white dark:bg-black shadow-md p-4 flex justify-around border-t dark:border-gray-500 md:hidden">
+    <nav className="fixed bottom-0 left-0 w-full bg-white dark:bg-black shadow-md px-4 flex justify-around border-t dark:border-gray-500 md:hidden">
       {navItems.map((item) => (
         <Link key={item.name} href={item.href}>
           <div
-            className={`flex flex-col items-center cursor-pointer transition-colors duration-200 ${
+            className={`flex flex-col items-center cursor-pointer transition-colors duration-200 w-[25vw] py-4 ${
               active === item.name
                 ? "text-green-500"
                 : "text-black dark:text-white"
